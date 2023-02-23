db9cf8b3e436c0fa83241c3adc21bd2259481ddb
duy-lvl <duylvlse160831@fpt.edu.vn>
2023-02-23 10:05:52 +0700

    first commit

---
+++ Assets/Script/PipeSpawnScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawnScript : MonoBehaviour
+{
+    public GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 0;
+    public float heightOffset = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPipe();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+            spawnPipe();
+        }
+        
+    }
+    void spawnPipe()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+        Vector3 position = new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0);
+        Instantiate(pipe, position, transform.rotation);
+    }
+}
