533dc6cc65238e3c262c426ec2c600b13b986214
duy-lvl <duylvlse160831@fpt.edu.vn>
2023-02-23 11:19:29 +0700

    first commit

--- Assets/Script/BirdScript.cs
+++ Assets/Script/BirdScript.cs
@@ -7,18 +7,33 @@ public class BirdScript : MonoBehaviour
     [SerializeField]
     int flapStrength;
     Rigidbody2D rb;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             rb.velocity = Vector2.up * flapStrength;
         }
+
+        if (transform.position.y < -5.5 || transform.position.y > 9)
+        {
+            birdIsAlive = false;
+            logic.gameOver();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive= false;
     }
 }
